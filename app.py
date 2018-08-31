ae69fda576e3d54362f0e7af4408b8f3c8d18bdf
Akshay Mahajan <akshay.mahajan@springernature.com>
2018-08-31 15:04:55 +0530

    Akshay- CSS Changes.

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-<<<<<<< Updated upstream
-def index():
-	return render_template('index.html')
-=======
+
 	def index():
 	    return render_template('index.html')
 
@@ -15,7 +12,6 @@ def index():
     	if request.method == 'POST':
         	textValue = request.form['textQuery'].lstrip().rstrip()
         	return render_template()
->>>>>>> Stashed changes
 
 if __name__ == '__main__':
 	app.run(debug=True)
